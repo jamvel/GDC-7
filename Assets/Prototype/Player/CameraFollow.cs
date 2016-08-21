2087d6f732fb5b2c2210c85cf07d7aee561059e3
Jamie Vella <jamie.vella.14@um.edu.mt>
2016-08-21 16:35:51 +0200

    Removed Import . Commit of player movement and smooth camera follow prototype

---
+++ Assets/Prototype/Player/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	public float dampTime = 0.15f;
+	private Vector3 velocity = Vector3.zero;
+	public Transform target;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (target){
+			Vector3 point = Camera.main.WorldToViewportPoint(target.position);
+			Vector3 delta = target.position - Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+			Vector3 destination = transform.position + delta;
+			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+		}
+
+	}
+}
---
+++ Assets/Prototype/Player/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 10f;
+	public float jumpVelocity = 10f;
+	public LayerMask playerMask;
+	public GameObject tagGroundLeft,tagGroundRight;
+	public bool airControl = true;
+
+	private Transform playerTransform,tagLeftTransform, tagRightTransform;
+	private Rigidbody2D playerRigidBody;
+	private bool isGround = false;
+
+	void Start(){
+		playerTransform = this.GetComponent<Transform> ();
+		tagLeftTransform = tagGroundLeft.GetComponent<Transform> ();
+		tagRightTransform = tagGroundRight.GetComponent<Transform> ();
+		playerRigidBody = this.GetComponent<Rigidbody2D> ();
+	}
+
+	void Update(){
+		//Horizontal Movement
+		Move (Input.GetAxisRaw ("Horizontal"));
+		if(Input.GetButtonDown("Jump")){ //vertical
+			if(isGround){
+				playerRigidBody.velocity = jumpVelocity * Vector2.up;
+			}
+		}
+
+	}
+
+	void FixedUpdate (){
+		Debug.DrawLine(playerTransform.position,tagLeftTransform.position);
+		Debug.DrawLine(playerTransform.position,tagRightTransform.position);
+		if(Physics2D.Linecast(playerTransform.position,tagLeftTransform.position,playerMask) || 
+			Physics2D.Linecast(playerTransform.position,tagRightTransform.position,playerMask)){ //Ground Check
+			isGround = true;
+		}else{
+			isGround = false;
+		}
+	}
+
+	public void Move(float horizontalInput){
+		if(!airControl && !isGround){
+			return;
+		}
+		Vector2 moveVelocity = playerRigidBody.velocity;
+		moveVelocity.x = horizontalInput * speed;
+		playerRigidBody.velocity = moveVelocity;
+	}
+}
