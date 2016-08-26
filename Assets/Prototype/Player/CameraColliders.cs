70bccffdba41ce21c271030d3ab7775aa72f88d4
Jamie Vella <jamie.vella.14@um.edu.mt>
2016-08-26 12:41:17 +0200

    Added camera snapping to side of rooms and camera locking. Camera self adjusts to different resolutions and aspect ratios. Added Camera layer, to be reserved for camera colliders etc. Modified project settings 2DPhysicsCollision Matrix to only allow objects in Camera layer to collide with objects in the same layer.

---
+++ Assets/Prototype/Player/CameraColliders.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraColliders : MonoBehaviour {
+	void OnTriggerEnter2D(Collider2D c){
+		Debug.Log ("Anchored to GameObject "+c.name);
+		Camera.main.GetComponent<CameraFollow> ().Anchor(gameObject.GetComponent<BoxCollider2D>(),"vertical");
+
+		//check collider name or tag to identify if vertical or horizontal
+
+	}
+}
