760a0298f376f312ac81d9caab122e1c3e3d7d0f
Jamie Vella <jamie.vella.14@um.edu.mt>
2016-09-19 08:51:58 +0200

    Reverted. Cleaned up project. Added new scene for pixel art . Added bounds and fps scripts. Added some folder for organisation

---
+++ Assets/Scripts/BoundEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor (typeof(Bound))]
+public class BoundEditor : Editor{
+	Bound b;
+	public override void OnInspectorGUI(){
+		GUILayout.BeginHorizontal();
+		GUILayout.Label(" X Size ");
+		b.x = EditorGUILayout.FloatField(b.x, GUILayout.Width(50));
+		GUILayout.EndHorizontal();
+		GUILayout.BeginHorizontal();
+		GUILayout.Label(" Y Size ");
+		b.y = EditorGUILayout.FloatField(b.y, GUILayout.Width(50));
+		GUILayout.EndHorizontal();
+		GUILayout.BeginHorizontal();
+		GUILayout.Label(" Scale ");
+		b.scale = EditorGUILayout.FloatField(b.scale, GUILayout.Width(50));
+		GUILayout.EndHorizontal();
+
+		SceneView.RepaintAll ();
+	}
+
+	public void OnEnable()
+	{
+		b = (Bound)target;
+	}
+}
