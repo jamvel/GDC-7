cefd16a311a3c4649437e224b90145fa8ca4b0a2
Karl Farrugia <karlfarrugia@live.com>
2016-10-02 18:38:58 +0200

    scripts not uploaded on last commit

---
+++ Assets/Scripts/EnemyBars.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBars : MonoBehaviour{
+    public int maxHealth = 100;
+    public int curHealth = 100;
+
+    public float healthBarLength;
+
+    // Use this for initialization
+    void Start(){
+        healthBarLength = Screen.width / 6;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        AddjustCurrentHealth(0);
+    }
+
+    void OnGUI(){
+        Vector2 targetPos;
+        targetPos = Camera.main.WorldToScreenPoint(transform.position);
+        GUI.Box(new Rect(targetPos.x, targetPos.y, healthBarLength, 20), curHealth + "/" + maxHealth);
+    }
+
+    public void AddjustCurrentHealth(int adj){
+        curHealth += adj;
+
+        if (curHealth < 0)
+            curHealth = 0;
+
+        if (curHealth > maxHealth)
+            curHealth = maxHealth;
+
+        if (maxHealth < 1)
+            maxHealth = 1;
+
+        healthBarLength = (Screen.width / 6) * (curHealth / (float)maxHealth);
+    }
+}
