f17ed7689baae37d30c121342bc6e0a0270c83b5
Karl Farrugia <karlfarrugia@live.com>
2016-10-18 16:36:29 +0200

    implemented buttons

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+    private bool paused = false;
+
+    public void exitButton(){
+        Application.Quit();
+    }
+
+    public void pauseButton(){
+        if(paused){
+            paused = false;
+            Time.timeScale = 1;
+            AudioListener.volume = 1;
+        }
+        else{
+            paused = true;
+            Time.timeScale = 0;
+            AudioListener.volume = 0;
+        }
+    }
+}
