8a92b5755e096917f74930e2b4b315bba3cbd2e1
Jamie Vella <jamie.vella.14@um.edu.mt>
2016-11-08 18:16:15 +0100

    Updated UI in respawn scene

---
+++ Assets/Scripts/UI_Respawn.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UI_Respawn : MonoBehaviour {
+    public Button option1;
+    public Button option2;
+
+    public EventSystem evtSystemRef;
+
+    int selectedId = 0;
+
+
+    void Update() {
+        if(evtSystemRef.currentSelectedGameObject == null) {
+            evtSystemRef.SetSelectedGameObject(option1.gameObject);
+            select(0);
+        }
+    }
+    public void select(int id) {
+        selectedId = id;
+        Debug.Log(selectedId);
+    }
+}
