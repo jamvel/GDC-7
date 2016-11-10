0c34ef09149b285d0fbcb7508a489670a16ebefd
Jonathan <jonathanborg1996@gmail.com>
2016-11-10 23:15:02 +0100

    Working on enemy to deal damage to player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,10 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Enemy : MonoBehaviour {
-
+    public float maxHealth = 100f;
+    public float currentHealth = 100f;
     public float health;
     public float damage;
+    //public Canvas UI_Canvas;
+
+    private SpriteRenderer sr;
+    private RectTransform healthbarRectTransform;
+    private Text healthRatioText;
+
+    private int i;
+    private Rigidbody2D rb;
+    private Transform tr;
 
     /*public void DamageEnemy (int damage) {
         enemyStats.health -= damage;
@@ -13,7 +24,42 @@ public class Enemy : MonoBehaviour {
         }
     }*/
     void Start(){
+        sr = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        tr = GetComponent<Transform>();
+        healthbarRectTransform = transform.Find("Enemy_Health_Bar").gameObject.transform.Find("Fill Area").gameObject.transform.Find("Fill").gameObject.GetComponent<RectTransform>(); //find healthbar
+        //healthRatioText = UI_Canvas.transform.Find("HealthBar_Background").gameObject.transform.Find("RatioText_HealthBar").gameObject.GetComponent<Text>(); //find ratio text for health bar
+        updateHealthBar();
 
+    }
+
+    public void updateHealthBar() {
+        float healthRatio = currentHealth / maxHealth;
+        healthbarRectTransform.localScale = new Vector3(healthRatio, 1, 1);
+        //healthRatioText.text = (healthRatio * 100).ToString() + "/" + maxHealth;
+    }
+
+    public void updateHealthBar(Damage dmg) { //updates current health and health bar
+        i = Random.Range(0, 2);
+
+        if (dmg.isRight) {
+            tr.position = Vector2.Lerp(tr.position, new Vector2(tr.position.x - 0.6f, tr.position.y), 0.5f);
+            Debug.Log("Enemy hit the player from the right");
+        } else {
+            tr.position = Vector2.Lerp(tr.position, new Vector2(tr.position.x + 0.6f, tr.position.y), 0.5f);
+            Debug.Log("Enemy hit the player from the left");
+        }
+        currentHealth -= dmg.damage;
+        updateHealthBar();
+        StartCoroutine(changeSpriteColor());
+
+    }
+    
+    private IEnumerator changeSpriteColor() {
+        Color original = new Color(255, 255, 255, 255);
+        sr.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        sr.color = original;
     }
 
     public static void killEnemy(Enemy enemy) {
