b2ac9036352ee64607fb833e464c12b5332c5edd
Jonathan <jonathanborg1996@gmail.com>
2016-11-19 04:07:38 +0100

    Added prefabs, modified some scripts and working on dealing damage to enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -30,9 +30,26 @@ public class Enemy : MonoBehaviour {
     public void updateHealthBar() {
         float healthRatio = currentHealth / maxHealth;
         healthbarRectTransform.localScale = new Vector3(healthRatio, 1, 1);
-        //healthRatioText.text = (healthRatio * 100).ToString() + "/" + maxHealth;
     }
-    
+
+    public void updateHealthBar(Damage dmg) { //updates current health and health bar
+        i = Random.Range(0, 2);
+        //grunt.clip = grunts[i];
+        //grunt.Play();
+
+        if (dmg.isRight) {
+            tr.position = Vector2.Lerp(tr.position, new Vector2(tr.position.x - 0.6f, tr.position.y), 0.5f);
+            Debug.Log("Player hit the enemy from the right");
+        } else {
+            tr.position = Vector2.Lerp(tr.position, new Vector2(tr.position.x + 0.6f, tr.position.y), 0.5f);
+            Debug.Log("Player hit the enemy from the left");
+        }
+        currentHealth -= dmg.damage;
+        updateHealthBar();
+        StartCoroutine(changeSpriteColor());
+
+    }
+
     private IEnumerator changeSpriteColor() {
         Color original = new Color(255, 255, 255, 255);
         sr.color = Color.red;
--- Assets/Scripts/HitTrigger.cs
+++ Assets/Scripts/HitTrigger.cs
@@ -7,8 +7,13 @@ public class HitTrigger : MonoBehaviour {
         if(c.tag == "Enemy") {
             transform.parent.gameObject.GetComponent<Player>().updateHealthBar(new Damage(isRight, c.gameObject.GetComponent<Enemy>().damage));
         }
+
         if (c.tag == "DamagePiece") {
             transform.parent.gameObject.GetComponent<Player>().updateHealthBar(new Damage(isRight, c.gameObject.GetComponent<DamageDealer>().damage));
+        }
+
+        if (c.tag == "PlayerSword") {
+            transform.parent.gameObject.GetComponent<Enemy>().updateHealthBar(new Damage(isRight, c.gameObject.GetComponent<DamageDealer>().damage));
         }
     }
 }
