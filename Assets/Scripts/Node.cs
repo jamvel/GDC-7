5ffd1548c01ca4f064f743800ae30e971342e793
Jamie Vella <jamie.vella.14@um.edu.mt>
2016-11-20 21:36:54 +0100

    Update Node script. Added Boss Node in level scene

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -6,37 +6,50 @@ public class Node : MonoBehaviour {
     public GameObject[] toNode; //stores where player goes to if player hits node trigger
     public bool isWest;
     public bool isNorth; //if touches north collider
+    public bool toBoss = false;
     [HideInInspector]public Transform camSpawn;
     [HideInInspector]public Transform playerSpawn;
 
     void Start() {
-        camSpawn = toNode[GameManager.instance.nodeIndex].transform.FindChild("Camera_Spawn");
-        playerSpawn = toNode[GameManager.instance.nodeIndex].transform.FindChild("Player_Spawn");
+        if (toNode != null) {
+            camSpawn = toNode[GameManager.instance.nodeIndex].transform.FindChild("Camera_Spawn");
+            playerSpawn = toNode[GameManager.instance.nodeIndex].transform.FindChild("Player_Spawn");
+        }
     }
 
     void OnTriggerEnter2D(Collider2D c) {
+        if(toNode.Length == 0 || toNode == null) {
+            return;
+        }
+
         if(c.gameObject.tag == "Player") {
-            Camera.main.GetComponent<CameraFollow>().lockCamera = true;
-
-            if (toNode[GameManager.instance.nodeIndex].GetComponent<Node>().isWest) {
-                c.gameObject.transform.position = new Vector3(playerSpawn.position.x, playerSpawn.position.y, toNode[GameManager.instance.nodeIndex].transform.position.z);
-            }
-            else {
-                c.gameObject.transform.position = new Vector3(playerSpawn.position.x, playerSpawn.position.y, toNode[GameManager.instance.nodeIndex].transform.position.z);
-            }
-            Camera.main.GetComponent<CameraFollow>().MoveCameraTo(new Vector3(camSpawn.position.x, camSpawn.position.y, -10));
-            Camera.main.GetComponent<CameraFollow>().anchorVertical = true;
-            if (toNode[GameManager.instance.nodeIndex].GetComponent<Node>().isWest) {
-                Camera.main.GetComponent<CameraFollow>().westCameraCollider = true;
+            if (toNode[GameManager.instance.nodeIndex].GetComponent<Node>().toBoss) {
+                Debug.Log("to Boss scene");
             }else {
-                Camera.main.GetComponent<CameraFollow>().eastCameraCollider = true;
-            }
+                Camera.main.GetComponent<CameraFollow>().lockCamera = true;
 
-            if (toNode[GameManager.instance.nodeIndex].GetComponent<Node>().isNorth) {
-                Camera.main.GetComponent<CameraFollow>().anchorHorziontal = true;
-                Camera.main.GetComponent<CameraFollow>().northCameraCollider = true;
+                if (toNode[GameManager.instance.nodeIndex].GetComponent<Node>().isWest) {
+                    c.gameObject.transform.position = new Vector3(playerSpawn.position.x, playerSpawn.position.y, toNode[GameManager.instance.nodeIndex].transform.position.z);
+                }
+                else {
+                    c.gameObject.transform.position = new Vector3(playerSpawn.position.x, playerSpawn.position.y, toNode[GameManager.instance.nodeIndex].transform.position.z);
+                }
+                Camera.main.GetComponent<CameraFollow>().MoveCameraTo(new Vector3(camSpawn.position.x, camSpawn.position.y, -10));
+                Camera.main.GetComponent<CameraFollow>().anchorVertical = true;
+                if (toNode[GameManager.instance.nodeIndex].GetComponent<Node>().isWest) {
+                    Camera.main.GetComponent<CameraFollow>().westCameraCollider = true;
+                }
+                else {
+                    Camera.main.GetComponent<CameraFollow>().eastCameraCollider = true;
+                }
+
+                if (toNode[GameManager.instance.nodeIndex].GetComponent<Node>().isNorth) {
+                    Camera.main.GetComponent<CameraFollow>().anchorHorziontal = true;
+                    Camera.main.GetComponent<CameraFollow>().northCameraCollider = true;
+                }
+                Camera.main.GetComponent<CameraFollow>().lockCamera = false;
             }
-            Camera.main.GetComponent<CameraFollow>().lockCamera = false;
+           
         }
     }
 }
